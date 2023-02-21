27101b70f902b38524fc832d171d32a81c790c3f
Matilda Bloom <114911401+mimmz111@users.noreply.github.com>
2023-02-21 14:35:12 +0100

    First page och javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+const SlideImg = document.getElementById("slide1")
+
 document.getElementById("listerner-right").addEventListener("click",displayDate)
 function displayDate(){
     console.log("hej")
+    SlideImg.src="../img/slide/slide2.jpg"
+}
+
+document.getElementById("listerner-left").addEventListener("click",displayDate)
+function displayDate(){
+    console.log("hejdå")
+    SlideImg.src="../img/slide/slide2.jpg"
 }
