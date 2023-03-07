8d9bd465e0a692e928d1779a1613db1f731ece6f
Matilda Bloom <114911401+mimmz111@users.noreply.github.com>
2023-03-07 14:38:39 +0100

    slide function works etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+img_src_array = ["img/slide/0_slide1.jpg", "img/slide/1_slide2.jpg"]
+
+function changePhotoRight() {
+    var length = document.getElementById('slide_img').src.length
+    
+    var index = eval(document.getElementById('slide_img').src[length - 12])
+    if (index < img_src_array.length - 1) {document.getElementById('slide_img').src = img_src_array[index + 1]}
+    else {document.getElementById('slide_img').src = img_src_array[0]}
+    
+}
+
+function changePhotoLeft() {
+    var length = document.getElementById('slide_img').src.length
+    
+    var index = eval(document.getElementById('slide_img').src[length - 12])
+    if (index > 0) {document.getElementById('slide_img').src = img_src_array[index - 1]}
+    else {document.getElementById('slide_img').src = img_src_array[img_src_array.length - 1]}
+        
+    
+}
+
+
+document.getElementById('listerner-left').addEventListener("click", changePhotoLeft)
+
+document.getElementById('listerner-right').addEventListener("click", changePhotoRight)
+
 function togglemenu(){
     document.getElementById('link-list').classList.toggle('link-list-visible')
     document.getElementById('burger').classList.toggle('burger-x')
@@ -5,24 +31,3 @@ function togglemenu(){
 
 }
 document.getElementById('burger').addEventListener('click', togglemenu)
-
-
-
-
-
-
-
-
-const SlideImg = document.getElementById("slide1")
-
-document.getElementById("listerner-right").addEventListener("click",displayDate)
-function displayDate(){
-    console.log("hej")
-    SlideImg.src="../img/slide/slide2.jpg"
-}
-
-document.getElementById("listerner-left").addEventListener("click",displayDate)
-function displayDate(){
-    console.log("hejdå")
-    SlideImg.src="../img/slide/slide2.jpg"
-}
