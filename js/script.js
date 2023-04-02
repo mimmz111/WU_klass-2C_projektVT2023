1baff08031ae420919e87337789610fce32d1c91
Matilda Bloom <114911401+mimmz111@users.noreply.github.com>
2023-04-02 22:19:04 +0200

    real final push

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 function togglemenu(){
     document.getElementById('link-list').classList.toggle('link-list-visible')
     document.getElementById('burger').classList.toggle('burger-x')
-
-
 }
 document.getElementById('burger').addEventListener('click', togglemenu)
 
 
-img_src_array = ["img/slide/0_slide1.jpg", "img/slide/1_slide2.jpg", "img/slide/2_slide3.jpg","img/slide/3_slide4.jpg","img/slide/4_slide5.jpg","img/slide/5_slide6.jpg","img/slide/6_slide7.jpg","img/slide/7_slide8.jpg","img/slide/8_slide9.jpg","img/slide/9_slide9.jpg"]
+img_src_array = ["img/slide/0_slide1.jpg", "img/slide/1_slide2.jpg", "img/slide/2_slide3.jpg","img/slide/3_slide4.jpg","img/slide/4_slide5.jpg","img/slide/5_slide6.jpg","img/slide/6_slide7.jpg","img/slide/7_slide8.jpg","img/slide/8_slide9.jpg","img/slide9_slide9.jpg"]
 
 function changePhotoRight() {
     var length = document.getElementById('slide_img').src.length
